0f77d468f9bc0a1f70c61ebbcd63ce700b11ecb7
Eric Biscondi <eric.biscondi@gmail.com>
2012-03-12 21:39:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passmontagne::Application.config.secret_token = '74890e20c97a326823ce139ded41d4bd0d6e9d865ac98555f7238b976f4c68085646e49dabe4b6f484a1c6a33ff7661980a985e17a68a0205937f1b0b06c6319'
