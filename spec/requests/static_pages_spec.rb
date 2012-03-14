2a7827fff587b35b6d99043c06442fd9a591d9ea
Eric Biscondi <eric.biscondi@gmail.com>
2012-03-14 21:36:42 +0100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,66 +1,30 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
 
   describe "Home page" do
-
-    it "should have the content of h1 to 'Pass Montagne'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Pass Montagne')
-    end
-
-    it "should have the content of title to 'Accueil'" do
-      visit '/static_pages/home'
-      page.should have_selector('title' ,
-                      :text => "Pass Montagne | Accueil")
-    end
-
+    before {visit root_path}
+    it { should have_selector('h1',    text: 'Pass Montagne') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Accueil' }
   end
   
   describe "Help page" do
-
-    it "should have the content of h1 to 'Aide'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Aide')
-    end
-
-    it "should have the content of title to 'Aide'" do
-      visit '/static_pages/help'
-      page.should have_selector('title' , 
-                    :text => "Pass Montagne | Aide")
-    end
-
+    before {visit help_path}
+    it {should have_selector('h1', text: 'Aide')}
+    it {should have_selector('title' , text: "Pass Montagne | Aide")}
   end
   
   describe "A propos" do
-
-    it "should have the content of h1 to 'A propos'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'A propos')
-    end
-
-    it "should have the content of title to 'A propos'" do
-      visit '/static_pages/about'
-      page.should have_selector('title' , :text => "Pass Montagne | A propos")
-    end
-
-
+    before {visit about_path}
+    it {should have_selector('h1', text: 'A propos')}
+    it {should have_selector('title' , text: "Pass Montagne | A propos")}
   end
 
   describe "Contact" do
-
-    it "should have the content of h1 to 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-    it "should have the content of title to 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title' , :text => "Pass Montagne | Contact")
-    end
-
-
+    before {visit contact_path}
+    it {should have_selector('h1', text: 'Contact')}
+    it {should have_selector('title' , text: "Pass Montagne | Contact")}
   end
-
-
 end
