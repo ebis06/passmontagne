286d93fa0ff3b52e6a1f1fc659bf32c52df22a1a
Eric Biscondi <eric.biscondi@gmail.com>
2012-03-14 22:27:36 +0100

    Make a basic USer model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 describe "StaticPages" do
   subject {page}
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before {visit root_path}
-    it { should have_selector('h1',    text: 'Pass Montagne') }
-    it { should have_selector('title', text: full_title('')) }
+    let(:heading) {'Pass Montagne'}
+    let(:page_title) {''}
+    
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Accueil' }
   end
   
@@ -27,4 +34,5 @@ describe "StaticPages" do
     it {should have_selector('h1', text: 'Contact')}
     it {should have_selector('title' , text: "Pass Montagne | Contact")}
   end
+
 end
