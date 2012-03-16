8e39ae814e69919a9920ebd01b66fe84f51ea7ae
Eric Biscondi <eric.biscondi@gmail.com>
2012-03-16 20:26:59 +0100

    Finished sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Bienvenue sur Pass Montagne!"
       redirect_to @user
     else
